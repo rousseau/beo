0b7b53e70aa4b0e5732735ede7f6ae39ac83ca60
François Rousseau <rousseau.f@gmail.com>
2021-05-07 10:15:39 +0200

    Update beo_tio_inference.py

--- beo_tio_inference.py
+++ beo_tio_inference.py
@@ -19,34 +19,27 @@ from beo_pl_nets import Unet
 
 import glob
 
-max_subjects = 50
-training_split_ratio = 0.9  # use 90% of samples for training, 10% for testing
+import argparse
 
-data_path = home+'/Sync/Data/DHCP/'
-output_path = home+'/Sync/Experiments/'
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Beo TorchIO inference')
+  parser.add_argument('-i', '--input', help='Input image', type=str, required=True)
+  parser.add_argument('-m', '--model', help='Pytorch model', type=str, required=True)
+  parser.add_argument('-o', '--output', help='Output image', type=str, required=True)
+  args = parser.parse_args()
 
-all_seg = glob.glob(data_path+'**/*fusion_space-T2w_dseg.nii.gz', recursive=True)
-all_t2s = glob.glob(data_path+'**/*desc-restore_T2w.nii.gz', recursive=True)
 
-all_seg = all_seg[:max_subjects]
-subjects = []
+  subjects = []
 
-for seg_file in all_seg:
-    id_subject = seg_file.split('/')[6].split('_')[0:2]
-    id_subject = id_subject[0]+'_'+id_subject[1]
-
-    t2_file = [s for s in all_t2s if id_subject in s][0]
-    
-    subject = tio.Subject(
-        t2=tio.ScalarImage(t2_file),
-        seg=tio.LabelMap(seg_file),
+  subject = tio.Subject(
+        t2=tio.ScalarImage(args.input),
     )
-    subjects.append(subject)
+  subjects.append(subject)
 
-dataset = tio.SubjectsDataset(subjects)
-print('Dataset size:', len(dataset), 'subjects')
+  dataset = tio.SubjectsDataset(subjects)
+  print('Dataset size:', len(dataset), 'subjects')
 
-training_transform = tio.Compose([
+  training_transform = tio.Compose([
   #tio.ToCanonical(),
   #tio.RandomMotion(p=0.2),
   #tio.RandomBiasField(p=0.3),
@@ -58,68 +51,53 @@ training_transform = tio.Compose([
   #  tio.RandomElasticDeformation(): 0.2,
   #}),
   tio.OneHot(),
-])
+  ])
 
-validation_transform = tio.Compose([
-  #tio.ToCanonical(),
-  tio.ZNormalization(masking_method=tio.ZNormalization.mean),
-  tio.OneHot(),
-])
+  validation_transform = tio.Compose([
+    #tio.ToCanonical(),
+    tio.ZNormalization(masking_method=tio.ZNormalization.mean),
+    tio.OneHot(),
+  ])
 
-num_subjects = len(dataset)
-num_training_subjects = int(training_split_ratio * num_subjects)
-num_validation_subjects = num_subjects - num_training_subjects
 
-num_split_subjects = num_training_subjects, num_validation_subjects
-training_subjects, validation_subjects = torch.utils.data.random_split(subjects, num_split_subjects)
+  validation_set = tio.SubjectsDataset(
+    subjects, transform=validation_transform )
 
-training_set = tio.SubjectsDataset(
-    training_subjects, transform=training_transform)
-
-validation_set = tio.SubjectsDataset(
-    validation_subjects, transform=validation_transform, )
-
-print('Training set:', len(training_set), 'subjects')
-print('Validation set:', len(validation_set), 'subjects')
+  print('Validation set:', len(validation_set), 'subjects')
 
 
 
-print('Inference')
+  print('Inference')
 
-patch_overlap = 64
-patch_size = 128
-batch_size = 2
+  patch_overlap = 64
+  patch_size = 128
+  batch_size = 2
 
-subject = validation_set[0]
+  subject = validation_set[0]
 
-grid_sampler = tio.inference.GridSampler(
-  subject,
-  patch_size,
-  patch_overlap,
-  )
+  grid_sampler = tio.inference.GridSampler(
+    subject,
+    patch_size,
+    patch_overlap,
+    )
 
-patch_loader = torch.utils.data.DataLoader(grid_sampler, batch_size=batch_size)
-aggregator = tio.inference.GridAggregator(sampler=grid_sampler, overlap_mode='average')
+  patch_loader = torch.utils.data.DataLoader(grid_sampler, batch_size=batch_size)
+  aggregator = tio.inference.GridAggregator(sampler=grid_sampler, overlap_mode='average')
 
-net = Unet.load_from_checkpoint(output_path+'example_model.ckpt')
-net.eval()
+  net = Unet.load_from_checkpoint(args.model)
+  net.eval()
 
-with torch.no_grad():
-  for patches_batch in patch_loader:
-    input_tensor = patches_batch['t2'][tio.DATA]
-    locations = patches_batch[tio.LOCATION]
-    outputs = net(input_tensor)
-    aggregator.add_batch(outputs, locations)
-output_tensor = aggregator.get_output_tensor()
+  with torch.no_grad():
+    for patches_batch in patch_loader:
+      input_tensor = patches_batch['t2'][tio.DATA]
+      locations = patches_batch[tio.LOCATION]
+      outputs = net(input_tensor)
+      aggregator.add_batch(outputs, locations)
+  output_tensor = aggregator.get_output_tensor()
+  output_tensor = torch.sigmoid(output_tensor)
 
-print(output_tensor.shape)
+  print(output_tensor.shape)
 
-print('Saving images')
-output_seg = tio.ScalarImage(tensor=output_tensor, affine=subject['t2'].affine)
-output_seg.save(output_path+'toto.nii.gz')
-subject['t2'].save(output_path+'toto_t2.nii.gz')
-subject['seg'].save(output_path+'toto_seg.nii.gz')
-
-print(output_seg.affine)
-print(subject['t2'].affine)
-print(subject['seg'].affine)
+  print('Saving images')
+  output_seg = tio.ScalarImage(tensor=output_tensor, affine=subject['t2'].affine)
+  output_seg.save(args.output)
