e86a0f796e982ccd1e7a8d4224054c7a671c6b83
François Rousseau <rousseau.f@gmail.com>
2021-05-12 17:13:50 +0200

    Update beo_pl_nets.py

--- beo_pl_nets.py
+++ beo_pl_nets.py
@@ -85,3 +85,111 @@ class Unet(pl.LightningModule):
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=1e-3)
+
+
+class Encoder(torch.nn.Module):
+  def __init__(self, latent_dim = 32, n_filters = 16, patch_size = 64):
+    super(Encoder, self).__init__()
+
+    n = 2*2*2    #3 layers of stride 2 : 64/8 * 64/8 * 64/8 * 16 -> 8192 hidden dim !
+    self.hidden_dim = int((patch_size / n)*(patch_size / n)*(patch_size / n)*n_filters)
+    self.latent_dim = int(latent_dim) 
+
+    self.enc = nn.Sequential(
+      nn.Conv3d(in_channels = 1, out_channels = n_filters, kernel_size = 3,stride = 2, padding=1),
+      nn.ReLU(),
+      nn.Conv3d(in_channels = n_filters, out_channels = n_filters, kernel_size = 3,stride = 2, padding=1),
+      nn.ReLU(),
+      nn.Conv3d(in_channels = n_filters, out_channels = n_filters, kernel_size = 3,stride = 2, padding=1),
+      nn.Tanh(),
+      nn.Flatten(),
+      nn.Linear(self.hidden_dim,self.latent_dim)
+      )
+
+  def forward(self,x):
+    return self.enc(x)
+
+class Feature(torch.nn.Module):
+  def __init__(self, n_filters = 16):
+    super(Feature, self).__init__()
+    self.feat = nn.Sequential(
+      nn.Conv3d(in_channels = 1, out_channels = n_filters, kernel_size = 3,stride = 1, padding=1),
+      nn.ReLU(),
+      nn.Conv3d(in_channels = n_filters, out_channels = n_filters, kernel_size = 3,stride = 1, padding=1),
+      nn.Tanh()
+      )
+  def forward(self,x):
+    return self.feat(x)    
+
+class Reconstruction(torch.nn.Module):
+  def __init__(self, in_channels, n_filters = 16):
+    super(Reconstruction, self).__init__()
+    
+    self.recon = nn.Sequential(
+      nn.Conv3d(in_channels = in_channels, out_channels = n_filters, kernel_size = 3,stride = 1, padding=1),
+      nn.ReLU(),
+      nn.Conv3d(in_channels = n_filters, out_channels = n_filters, kernel_size = 3,stride = 1, padding=1),
+      nn.ReLU(),
+      nn.Conv3d(in_channels = n_filters, out_channels = 1, kernel_size = 3,stride = 1, padding=1)
+      )      
+  def forward(self,x):
+    return self.recon(x)    
+
+class DecompNet(pl.LightningModule):
+
+  def __init__(self, latent_dim = 32, n_filters = 16, patch_size = 64):
+    super().__init__()
+    self.patch_size = patch_size
+    n = 2*2*2    #3 layers of stride 2 : 64/8 * 64/8 * 64/8 * 16 -> 8192 hidden dim !
+    self.hidden_dim = int((patch_size / n)*(patch_size / n)*(patch_size / n)*n_filters)
+    self.latent_dim = int(latent_dim) 
+
+    self.encoder = Encoder(latent_dim, n_filters, patch_size)
+    self.feature = Feature(n_filters)
+    self.reconstruction = Reconstruction(n_filters+self.latent_dim, n_filters)
+    
+
+  def forward(self,x,y): 
+    zx = self.encoder(x)
+    fx = self.feature(x)
+    #zfx= self.latent2feature(zx)
+    zx = zx.view(-1,self.latent_dim,1,1)
+    zfx = zx.repeat(1,1,self.patch_size,self.patch_size,self.patch_size)
+    #x_hat = self.decoder(zx)
+    fxzfx = torch.cat([fx,zfx], dim=1)
+    x_hat = self.reconstruction(fxzfx)
+    
+    zy = self.encoder(y)
+    fy = self.feature(y)
+    #zfy= self.latent2feature(zy)
+    zy = zy.view(-1,self.latent_dim,1,1)    
+    zfy = zy.repeat(1,1,self.patch_size,self.patch_size,self.patch_size)
+    #y_hat = self.decoder(zy)
+    fyzfy = torch.cat([fy,zfy], dim=1)
+    y_hat = self.reconstruction(fyzfy)
+
+    #reconstruction of x using fy and zfx
+    fyzfx = torch.cat([fy,zfx], dim=1)
+    rx = self.reconstruction(fyzfx)
+
+    #reconstruction of y using fx and zfy
+    fxzfy = torch.cat([fx,zfy], dim=1)
+    ry = self.reconstruction(fxzfy)
+
+    #Add cycle consistency ?
+    
+    return x_hat, y_hat, rx, ry, fx, fy
+
+  def training_step(self, batch, batch_idx):
+    patches_batch = batch
+    x = patches_batch['t1'][tio.DATA]
+    y = patches_batch['t2'][tio.DATA]
+    
+    x_hat, y_hat, rx, ry, fx, fy = self(x,y)
+    loss = F.mse_loss(x_hat, x) + F.mse_loss(y_hat, y) + F.mse_loss(rx, x) + F.mse_loss(ry, y) + F.mse_loss(fx, fy)
+    self.log('train_loss', loss)
+    return loss
+
+  def configure_optimizers(self):
+    optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+    return optimizer
