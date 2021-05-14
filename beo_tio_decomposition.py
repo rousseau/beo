ab85e190c2e259fb45d93dfcbb5a24bc08c66105
rousseau <rousseau.f@gmail.com>
2021-05-14 18:07:46 +0200

    Update beo_tio_decomposition.py

--- beo_tio_decomposition.py
+++ beo_tio_decomposition.py
@@ -19,7 +19,9 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 import glob
 import multiprocessing
 
-max_subjects = 100
+from beo_pl_nets import DecompNet
+
+max_subjects = 10
 training_split_ratio = 0.9  # use 90% of samples for training, 10% for testing
 num_epochs = 5
 num_workers = 0#multiprocessing.cpu_count()
@@ -27,7 +29,7 @@ num_workers = 0#multiprocessing.cpu_count()
 training_batch_size = 1
 validation_batch_size = 1 
 
-patch_size = 128
+patch_size = 64
 samples_per_volume = 32
 max_queue_length = 256
 
@@ -85,3 +87,77 @@ training_transform = tio.Compose(transforms)
 validation_transform = tio.Compose([normalization, onehot])
 
 #%%
+seed = 42  # for reproducibility
+
+
+num_subjects = len(dataset)
+num_training_subjects = int(training_split_ratio * num_subjects)
+num_validation_subjects = num_subjects - num_training_subjects
+
+num_split_subjects = num_training_subjects, num_validation_subjects
+training_subjects, validation_subjects = torch.utils.data.random_split(subjects, num_split_subjects)
+
+training_set = tio.SubjectsDataset(
+    training_subjects, transform=training_transform)
+
+validation_set = tio.SubjectsDataset(
+    validation_subjects, transform=validation_transform)
+
+print('Training set:', len(training_set), 'subjects')
+print('Validation set:', len(validation_set), 'subjects')
+
+#%%
+
+print('num_workers : '+str(num_workers))
+
+probabilities = {0: 0, 1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1}
+#sampler = tio.data.UniformSampler(patch_size)
+sampler = tio.data.LabelSampler(
+          patch_size=patch_size,
+          label_name='seg',
+          label_probabilities=probabilities,
+)
+
+patches_training_set = tio.Queue(
+    subjects_dataset=training_set,
+    max_length=max_queue_length,
+    samples_per_volume=samples_per_volume,
+    sampler=sampler,
+    num_workers=num_workers,
+    shuffle_subjects=True,
+    shuffle_patches=True,
+)
+
+patches_validation_set = tio.Queue(
+    subjects_dataset=validation_set,
+    max_length=max_queue_length,
+    samples_per_volume=samples_per_volume,
+    sampler=sampler,
+    num_workers=num_workers,
+    shuffle_subjects=False,
+    shuffle_patches=False,
+)
+
+training_loader_patches = torch.utils.data.DataLoader(
+    patches_training_set, batch_size=training_batch_size)
+
+validation_loader_patches = torch.utils.data.DataLoader(
+    patches_validation_set, batch_size=validation_batch_size)
+
+#%%
+
+net = DecompNet()
+
+checkpoint_callback = ModelCheckpoint(
+  dirpath=output_path,
+  filename=prefix+'_{epoch:02d}',
+  verbose=True
+  )
+
+trainer = pl.Trainer(gpus=0, max_epochs=num_epochs, progress_bar_refresh_rate=20, callbacks=[checkpoint_callback])
+
+#%%
+
+trainer.fit(net, training_loader_patches, validation_loader_patches)
+
+print('Finished Training')
