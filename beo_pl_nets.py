a11959aec056eaf4d94c67986406e09ea139a232
François Rousseau <rousseau.f@gmail.com>
2021-05-17 13:35:18 +0200

    Update beo_pl_nets.py

--- beo_pl_nets.py
+++ beo_pl_nets.py
@@ -15,12 +15,12 @@ import torchio as tio
 
 
 class Unet(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, n_channels = 1, n_classes = 10, n_features = 32):
         super(Unet, self).__init__()
 
-        self.n_channels = 1
-        self.n_classes = 10
-        self.n_features = 32
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.n_features = n_features
 
         def double_conv(in_channels, out_channels):
             return nn.Sequential(
@@ -116,61 +116,14 @@ class Encoder(torch.nn.Module):
     return self.enc(x)
 
 class Feature(torch.nn.Module):
-  def __init__(self, n_filters = 16):
+  def __init__(self, n_channels = 1, n_features = 10, n_filters = 32):
     super(Feature, self).__init__()
 
-    self.n_channels = 1
-    self.n_classes = n_filters
-    self.n_features = n_filters
-
-    def double_conv(in_channels, out_channels):
-        return nn.Sequential(
-            nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm3d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm3d(out_channels),
-            nn.ReLU(inplace=True),
-        )
-
-    self.dc1 = double_conv(self.n_channels, self.n_features)
-    self.dc2 = double_conv(self.n_features, self.n_features*2)
-    self.dc3 = double_conv(self.n_features*2, self.n_features*4)
-    self.dc4 = double_conv(self.n_features*6, self.n_features*2)
-    self.dc5 = double_conv(self.n_features*3, self.n_features)
-
-    self.mp = nn.MaxPool3d(2)
-    self.up = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=True)
-
-    self.out = nn.Conv3d(self.n_features, self.n_classes, kernel_size=1)
-
-    # self.feat = nn.Sequential(
-    #   nn.Conv3d(in_channels = 1, out_channels = n_filters, kernel_size = 3,stride = 1, padding=1),
-    #   nn.ReLU(),
-    #   nn.Conv3d(in_channels = n_filters, out_channels = n_filters, kernel_size = 3,stride = 1, padding=1),
-    #   nn.ReLU(),
-    #   nn.Conv3d(in_channels = n_filters, out_channels = n_filters, kernel_size = 3,stride = 1, padding=1),
-    #   nn.Tanh()
-    #   )
+    self.unet = Unet(n_channels = n_channels, n_classes = n_features, n_features = n_filters)
 
   def forward(self,x):
-    #return self.feat(x)
-    x1 = self.dc1(x)
-
-    x2 = self.mp(x1)
-    x2 = self.dc2(x2)
-
-    x3 = self.mp(x2)
-    x3 = self.dc3(x3)
-
-    x4 = self.up(x3)
-    x4 = torch.cat([x4,x2], dim=1)
-    x4 = self.dc4(x4)
-
-    x5 = self.up(x4)
-    x5 = torch.cat([x5,x1], dim=1)
-    x5 = self.dc5(x5)
-    return self.out(x5)
+    xout = self.unet(x)
+    return nn.Tanh()(xout)
     
 
 class Reconstruction(torch.nn.Module):
@@ -203,48 +156,52 @@ class DecompNet(pl.LightningModule):
   def __init__(self, latent_dim = 32, n_filters = 16, patch_size = 64):
     super().__init__()
     self.patch_size = patch_size
-    n = 2*2*2    #3 layers of stride 2 : 64/8 * 64/8 * 64/8 * 16 -> 8192 hidden dim !
-    self.hidden_dim = int((patch_size / n)*(patch_size / n)*(patch_size / n)*n_filters)
     self.latent_dim = int(latent_dim) 
     self.n_classes = 10
 
     self.encoder = Encoder(latent_dim, n_filters, patch_size)
-    self.feature = Feature(n_filters)
+    self.feature = Feature(1, n_filters, n_filters)
     self.reconstruction = Reconstruction(n_filters+self.latent_dim, n_filters)
     self.segmenter = Feature2Segmentation(n_filters, self.n_classes)
 
   def forward(self,x,y): 
-    zx = self.encoder(x)
-    fx = self.feature(x)
-    #zfx= self.latent2feature(zx)
-    zx = zx.view(-1,self.latent_dim,1,1,1)
-    zfx = zx.repeat(1,1,self.patch_size,self.patch_size,self.patch_size)
-    #x_hat = self.decoder(zx)
-    fxzfx = torch.cat([fx,zfx], dim=1)
-    x_hat = self.reconstruction(fxzfx)
-    
-    zy = self.encoder(y)
+
     fy = self.feature(y)
-    #zfy= self.latent2feature(zy)
-    zy = zy.view(-1,self.latent_dim,1,1,1)    
-    zfy = zy.repeat(1,1,self.patch_size,self.patch_size,self.patch_size)
-    #y_hat = self.decoder(zy)
-    fyzfy = torch.cat([fy,zfy], dim=1)
-    y_hat = self.reconstruction(fyzfy)
-
-    #reconstruction of x using fy and zfx
-    fyzfx = torch.cat([fy,zfx], dim=1)
-    rx = self.reconstruction(fyzfx)
-
-    #reconstruction of y using fx and zfy
-    fxzfy = torch.cat([fx,zfy], dim=1)
-    ry = self.reconstruction(fxzfy)
-
-    #Add cycle consistency ?
-
-    #Segmentation
-    sx = self.segmenter(fx)
     sy = self.segmenter(fy)
+    fx = self.feature(x)
+    sx = self.segmenter(fx)
+    x_hat = x
+    y_hat = y
+    rx = x
+    ry = y
+    
+    # zx = self.encoder(x)
+    # fx = self.feature(x)
+    # zx = zx.view(-1,self.latent_dim,1,1,1)
+    # zfx = zx.repeat(1,1,self.patch_size,self.patch_size,self.patch_size)
+    # fxzfx = torch.cat([fx,zfx], dim=1)
+    # x_hat = self.reconstruction(fxzfx)
+    
+    # zy = self.encoder(y)
+    # fy = self.feature(y)
+    # zy = zy.view(-1,self.latent_dim,1,1,1)    
+    # zfy = zy.repeat(1,1,self.patch_size,self.patch_size,self.patch_size)
+    # fyzfy = torch.cat([fy,zfy], dim=1)
+    # y_hat = self.reconstruction(fyzfy)
+
+    # #reconstruction of x using fy and zfx
+    # fyzfx = torch.cat([fy,zfx], dim=1)
+    # rx = self.reconstruction(fyzfx)
+
+    # #reconstruction of y using fx and zfy
+    # fxzfy = torch.cat([fx,zfy], dim=1)
+    # ry = self.reconstruction(fxzfy)
+
+    # #Add cycle consistency ?
+
+    # #Segmentation
+    # sx = self.segmenter(fx)
+    # sy = self.segmenter(fy)
     
     return x_hat, y_hat, rx, ry, fx, fy, sx, sy
 
@@ -256,7 +213,9 @@ class DecompNet(pl.LightningModule):
     
     x_hat, y_hat, rx, ry, fx, fy, sx, sy = self(x,y)
 
-    loss = F.mse_loss(x_hat, x) + F.mse_loss(y_hat, y) + F.mse_loss(rx, x) + F.mse_loss(ry, y) + F.mse_loss(fx, fy) + F.mse_loss(sx, s.float()) + F.mse_loss(sy, s.float())
+    #loss = F.mse_loss(x_hat, x) + F.mse_loss(y_hat, y) + F.mse_loss(rx, x) + F.mse_loss(ry, y) + F.mse_loss(fx, fy) + F.mse_loss(sx, s.float()) + F.mse_loss(sy, s.float())
+    loss = F.mse_loss(sx, s.float()) + F.mse_loss(sy, s.float())
+    
     self.log('train_loss', loss)
     return loss
 
