c2c24940c74d7a76fc1ba1fa061f35996bb7189f
François Rousseau <rousseau.f@gmail.com>
2023-02-03 16:40:12 +0100

    Create beo_tcnn.py

---
+++ beo_tcnn.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import nibabel as nib
+import numpy as np
+import torch
+import pytorch_lightning as pl
+import torch.nn as nn
+import torch.nn.functional as F
+import argparse
