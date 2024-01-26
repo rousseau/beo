74e8ee6b8ff44f59f9dfda4fe74b0d48741d9065
François Rousseau <rousseau.f@gmail.com>
2024-01-26 15:11:04 +0100

    Update beo_reg_minimal.py

--- src/beo/beo_reg_minimal.py
+++ src/beo/beo_reg_minimal.py
@@ -26,11 +26,20 @@ class TwoDataSet(Dataset):
 
 
 
+#%% Main program
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Beo Registration 3D Image Pair')
-    parser.add_argument('-t', help='Target / Reference Image', type=str, required = True)
-    parser.add_argument('-s', help='Source / Moving Image', type=str, required = True)
+    parser.add_argument('-t', '--target', help='Target / Reference Image', type=str, required = True)
+    parser.add_argument('-s', '--source', help='Source / Moving Image', type=str, required = True)
 
     args = parser.parse_args()
 
+    target_image = nibabel.load(args.target)
+    source_image = nibabel.load(args.source)
+
+    target_data  = torch.Tensor(target_image.get_fdata())
+    source_data  = torch.Tensor(source_image.get_fdata())
+
+    target_data = torch.unsqueeze(torch.unsqueeze(target_data, 0),0)
+    source_data = torch.unsqueeze(torch.unsqueeze(source_data, 0),0)
 
