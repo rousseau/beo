bf5517974952fdc449a42d65ca0697c540c62a15
François Rousseau <rousseau.f@gmail.com>
2024-02-04 15:59:49 +0100

    Update beo_reg_minimal.py

--- src/beo/beo_reg_minimal.py
+++ src/beo/beo_reg_minimal.py
@@ -10,9 +10,130 @@ import pytorch_lightning as pl
 
 import torchio as tio
 import monai
+import math
+import numpy as np
 
 # Net modules
+class Unet(nn.Module):
+  def __init__(self, n_channels = 2, n_classes = 3, n_features = 8):
+    super(Unet, self).__init__()
+
+    self.n_channels = n_channels
+    self.n_classes = n_classes
+    self.n_features = n_features
+
+    def double_conv(in_channels, out_channels):
+      return nn.Sequential(
+        nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1),
+        nn.BatchNorm3d(out_channels),
+        nn.ReLU(inplace=True),
+        nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
+        nn.BatchNorm3d(out_channels),
+        nn.ReLU(inplace=True),
+        nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
+        nn.BatchNorm3d(out_channels),
+        nn.ReLU(inplace=True),
+      )
+
+    self.dc1 = double_conv(self.n_channels, self.n_features)
+    self.dc2 = double_conv(self.n_features, self.n_features)
+    self.dc3 = double_conv(self.n_features, self.n_features)
+    self.dc4 = double_conv(self.n_features*2, self.n_features)
+    self.dc5 = double_conv(self.n_features*2, self.n_features)
+    
+    self.ap = nn.AvgPool3d(2)
+
+    self.up = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=True)
+
+    self.x3_out = nn.Conv3d(self.n_features, self.n_classes, kernel_size=1)
+    self.x4_out = nn.Conv3d(self.n_features, self.n_classes, kernel_size=1)
+
+    self.out = nn.Conv3d(self.n_features, self.n_classes, kernel_size=1)
+
+  def forward(self, x):
+    x1 = self.dc1(x)
+
+    x2 = self.ap(x1)
+    x2 = self.dc2(x2)
+
+    x3 = self.ap(x2)
+    x3 = self.dc3(x3)
+
+    x4 = self.up(x3)
+    x4 = torch.cat([x4,x2], dim=1)
+    x4 = self.dc4(x4)
+
+    x5 = self.up(x4)
+    x5 = torch.cat([x5,x1], dim=1)
+    x5 = self.dc5(x5)
+    return self.out(x5)
+
+
     # code from voxelmorph repo
+
+class NCC(nn.Module):
+  """
+  Local (over window) normalized cross correlation loss.
+  """
+
+  def __init__(self, win=None):
+    super().__init__()
+    self.win = win
+
+  def forward(self, y_true, y_pred):
+
+    Ii = y_true
+    Ji = y_pred
+
+    # get dimension of volume
+    # assumes Ii, Ji are sized [batch_size, *vol_shape, nb_feats]
+    ndims = len(list(Ii.size())) - 2
+    assert ndims in [1, 2, 3], "volumes should be 1 to 3 dimensions. found: %d" % ndims
+
+    # set window size
+    win = [9] * ndims if self.win is None else self.win
+
+    # compute filters
+    sum_filt = torch.ones([1, 1, *win]).to("cuda")
+
+    pad_no = math.floor(win[0] / 2)
+
+    if ndims == 1:
+      stride = (1)
+      padding = (pad_no)
+    elif ndims == 2:
+      stride = (1, 1)
+      padding = (pad_no, pad_no)
+    else:
+      stride = (1, 1, 1)
+      padding = (pad_no, pad_no, pad_no)
+
+    # get convolution function
+    conv_fn = getattr(F, 'conv%dd' % ndims)
+
+    # compute CC squares
+    I2 = Ii * Ii
+    J2 = Ji * Ji
+    IJ = Ii * Ji
+
+    I_sum = conv_fn(Ii, sum_filt, stride=stride, padding=padding)
+    J_sum = conv_fn(Ji, sum_filt, stride=stride, padding=padding)
+    I2_sum = conv_fn(I2, sum_filt, stride=stride, padding=padding)
+    J2_sum = conv_fn(J2, sum_filt, stride=stride, padding=padding)
+    IJ_sum = conv_fn(IJ, sum_filt, stride=stride, padding=padding)
+
+    win_size = np.prod(win)
+    u_I = I_sum / win_size
+    u_J = J_sum / win_size
+
+    cross = IJ_sum - u_J * I_sum - u_I * J_sum + u_I * u_J * win_size
+    I_var = I2_sum - 2 * u_I * I_sum + u_I * u_I * win_size
+    J_var = J2_sum - 2 * u_J * J_sum + u_J * u_J * win_size
+
+    cc = cross * cross / (I_var * J_var + 1e-5)
+
+    return -torch.mean(cc)
+
 class SpatialTransformer(nn.Module):
     """
     N-D Spatial Transformer
@@ -82,18 +203,26 @@ class meta_registration_model(pl.LightningModule):
     def __init__(self, shape, int_steps = 7):  
         super().__init__()  
         self.shape = shape
-        self.unet = monai.networks.nets.Unet(spatial_dims=3, in_channels=2, out_channels=3, channels=(8,16,32), strides=(2,2))
+        #self.unet = monai.networks.nets.Unet(spatial_dims=3, in_channels=2, out_channels=3, channels=(8,16,32), strides=(2,2))
+        self.unet = Unet(n_channels = 2, n_classes = 3, n_features = 32)
         self.transformer = SpatialTransformer(size=shape)
         self.int_steps = int_steps
         self.vecint = VecInt(inshape=shape, nsteps=int_steps)
-        self.loss = nn.MSELoss()
+        self.loss = NCC()
+        #self.loss = nn.MSELoss()
+        #self.loss = monai.losses.LocalNormalizedCrossCorrelationLoss()
 
-    def forward(self,target,source):
-        x = torch.cat([target,source], dim=1)
+    def forward(self,source,target):
+        x = torch.cat([source,target], dim=1)
         forward_velocity = self.unet(x)
         forward_flow = self.vecint(forward_velocity)
         warped_source = self.transformer(source, forward_flow)
-        return warped_source
+
+        backward_velocity = - forward_velocity
+        backward_flow = self.vecint(backward_velocity)
+        warped_target = self.transformer(target, backward_flow)
+
+        return warped_source, warped_target
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=0.0001)
@@ -103,8 +232,8 @@ class meta_registration_model(pl.LightningModule):
 
         target = batch['target'][tio.DATA]
         source = batch['source'][tio.DATA]
-        warped_source = self(target,source)
-        return self.loss(target,warped_source)
+        warped_source, warped_target = self(source,target)
+        return self.loss(target,warped_source) + self.loss(source,warped_target)
 
 
 #%% Main program
@@ -125,19 +254,21 @@ if __name__ == '__main__':
     subjects.append(subject) 
 
     normalization = tio.ZNormalization()
-    resize = tio.Resize(128)
-    transforms = [resize, normalization]
+    #resize = tio.Resize(128)
+    #transforms = [resize, normalization]
+    transforms = [normalization]
     training_transform = tio.Compose(transforms)
 
     training_set = tio.SubjectsDataset(subjects, transform=training_transform)
     training_loader = torch.utils.data.DataLoader(training_set, batch_size=1)
 #%%
     # get the spatial dimension of the data (3D)
-    in_shape = resize(subjects[0]).target.shape[1:] 
+    #in_shape = resize(subjects[0]).target.shape[1:] 
+    in_shape = subjects[0].target.shape[1:]     
     reg_net = meta_registration_model(shape=in_shape)
 
 
-    trainer_reg = pl.Trainer(max_epochs=args.epochs,accelerator="cpu")   
+    trainer_reg = pl.Trainer(max_epochs=args.epochs, logger=False, enable_checkpointing=False)   
     trainer_reg.fit(reg_net, training_loader)  
 
 #%%
@@ -145,6 +276,11 @@ if __name__ == '__main__':
     inference_subject = training_transform(subject)
     source_data = torch.unsqueeze(inference_subject.source.data,0)
     target_data = torch.unsqueeze(inference_subject.target.data,0)    
-    warped_source = reg_net.forward(target_data,source_data)
+    warped_source,warped_target = reg_net.forward(source_data,target_data)
     o = tio.ScalarImage(tensor=warped_source[0].detach().numpy(), affine=inference_subject.source.affine)
     o.save(args.output)
+
+    #o = tio.ScalarImage(tensor=inference_subject.source.data.detach().numpy(), affine=inference_subject.source.affine)
+    #o.save('source.nii.gz')
+    #o = tio.ScalarImage(tensor=inference_subject.target.data.detach().numpy(), affine=inference_subject.target.affine)
+    #o.save('target.nii.gz')    
