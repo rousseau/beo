22995aa6a6bf60b94a53e9c8c16ee388bcd55b61
François Rousseau <rousseau.f@gmail.com>
2024-03-14 13:33:04 +0100

    Update beo_inference_pairwise_registration.py

--- src/beo/beo_inference_pairwise_registration.py
+++ src/beo/beo_inference_pairwise_registration.py
@@ -39,7 +39,7 @@ class meta_registration_model(pl.LightningModule):
         backward_flow = self.vecint(backward_velocity)
         warped_target = self.transformer(target, backward_flow)
 
-        return warped_source, warped_target
+        return warped_source, warped_target, forward_flow, backward_flow
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=0.0001)
@@ -58,34 +58,20 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Beo Registration 3D Image Pair')
     parser.add_argument('-t', '--target', help='Target / Reference Image', type=str, required = True)
     parser.add_argument('-s', '--source', help='Source / Moving Image', type=str, required = True)
-    parser.add_argument('--target_mask', help='Target / Reference Mask', type=str, required = True)
-    parser.add_argument('--source_mask', help='Source / Moving Mask', type=str, required = True)
-    parser.add_argument('--warped_target', help='Deformed Target / Reference Image', type=str, required = True)
-    parser.add_argument('--warped_source', help='Deformed Source / Moving Image', type=str, required = True)
+    parser.add_argument('-o', '--output', help='Output prefix filename', type=str, required = True)
 
     parser.add_argument('--load_unet', help='Input unet model', type=str, required = True)
-    parser.add_argument('--size', help='Image size', type=int, required = False, default=256)
+    parser.add_argument('--size', help='Image size', type=int, required = False, default=128)
 
     args = parser.parse_args()
 
     subjects = []
-    target = tio.Subject(
+    inference_target = tio.Subject(
         image=tio.ScalarImage(args.target),
-        mask=tio.LabelMap(args.target_mask),
     )
-    source = tio.Subject(
+    inference_source = tio.Subject(
         image=tio.ScalarImage(args.source),
-        mask=tio.LabelMap(args.source_mask),
     )
-    subjects.append(target) 
-    subjects.append(source)
-
-    masking = tio.Mask(masking_method='mask')
-    normalization = tio.ZNormalization(masking_method='mask')    
-    croporpad =tio.transforms.CropOrPad(mask_name='mask')
-    resize = tio.Resize(args.size)
-    transforms = [masking,normalization,croporpad,resize]
-    composed_transform = tio.Compose(transforms)
 
 #%%
     # get the spatial dimension of the data (3D)
@@ -95,23 +81,21 @@ if __name__ == '__main__':
 
 #%%
     print('Inference')
-    inference_target = composed_transform(target)
-    inference_source = composed_transform(source)
 
     target_data = torch.unsqueeze(inference_target.image.data,0)    
     source_data = torch.unsqueeze(inference_source.image.data,0)
 
-    warped_source,warped_target = reg_net.forward(source_data,target_data)
+    warped_source, warped_target, forward_flow, backward_flow = reg_net.forward(source_data,target_data)
+
 
 #%%
     print('Saving results')
     o = tio.ScalarImage(tensor=warped_source[0].detach().numpy(), affine=inference_target.image.affine)
-    o.save(args.warped_source)
+    o.save(args.output+'_warped.nii.gz')
+    o = tio.ScalarImage(tensor=warped_target[0].detach().numpy(), affine=inference_target.image.affine)   
+    o.save(args.output+'_inverse_warped.nii.gz')
+    o = tio.ScalarImage(tensor=forward_flow[0].detach().numpy(), affine=inference_target.image.affine)   
+    o.save(args.output+'_warp.nii.gz')
+    o = tio.ScalarImage(tensor=backward_flow[0].detach().numpy(), affine=inference_target.image.affine)   
+    o.save(args.output+'_inverse_warp.nii.gz')
 
-    o = tio.ScalarImage(tensor=warped_target[0].detach().numpy(), affine=inference_source.image.affine)
-    o.save(args.warped_target)
-
-    o = tio.ScalarImage(tensor=inference_source.image.data.detach().numpy(), affine=inference_source.image.affine)
-    o.save('source.nii.gz')
-    o = tio.ScalarImage(tensor=inference_target.image.data.detach().numpy(), affine=inference_target.image.affine)
-    o.save('target.nii.gz')    
